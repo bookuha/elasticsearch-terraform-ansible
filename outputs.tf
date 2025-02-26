91335e112ddecded11f09e9cc72480d9f5fac432
Bookuha <bookuha@gmail.com>
2025-02-26 19:42:48 +0200

    Fix outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "ec2_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = module.ec2.public_ip
+output "public_ip_ansible" {
+  description = "Public IP of the EC2 Ansible instance"
+  value       = module.ec2.public_ip_ansible
+}
+
+output "public_ip_elastic" {
+  description = "Public IP of the EC2 Elastic instance"
+  value       = module.ec2.public_ip_elastic
 }
